e649ba314f078474a04a75567045c724edc29c4d
paulfantom <pawel@krupa.net.pl>
2018-11-05 13:39:00 +0100

    add rootfs

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+
+ls
 docker build -t centos_7-baremetal_1 .
 docker save centos_7-baremetal_1 > centos_7-baremetal_1.tar
 packet-save2image < centos_7-baremetal_1.tar > image.tar.gz
